9ea8085761ee0e59ddae13d717914fd56a5aa0ef
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-24 17:35:57 +0700

    Update prereq minimum version Perinci::Sub::Gen::AccessTable to 0.55 (incompatible with previous version)

--- lib/Business/BankList/Indonesia.pm
+++ lib/Business/BankList/Indonesia.pm
@@ -4,7 +4,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-use Perinci::Sub::Gen::AccessTable 0.14 qw(gen_read_table_func);
+use Perinci::Sub::Gen::AccessTable qw(gen_read_table_func);
 
 use Exporter;
 our @ISA = qw(Exporter);
